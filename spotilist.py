782209b3b6aefba63f2055bb466e296e32a59fda
raz <zmazar@gmail.com>
2022-04-02 14:10:55 -0700

    Retrieve raw tracks for each playlist
    Implemented crude implementation for retrieving all tracks for a playlist.
    Added `Playlist` object to parse playlist information.

--- spotilist.py
+++ spotilist.py
@@ -28,6 +28,92 @@ API_CLIENT_SECRET = "2ec47cef47a4434b9921447f075f1b36"
 SPOTIFY_BASE_URL='https://api.spotify.com/v1'
 SPOTIFY_TOKEN_URL='https://accounts.spotify.com/api/token'
 
+class Playlist(object):
+    """Playlist Class
+
+    A playlist class takes in a JSON formatted item, and parses it to create a 
+    usable data object.
+
+    Attributes
+    ----------
+    description : str
+        Description of the playlist
+    name : str
+        Name of the playlist
+    owner : str
+        Display name of the owner of the playlist
+    tracks : list
+        List of Track objects containing information about the songs on the
+        playlist
+    tracks_size : int
+        Number of tracks in the playlist
+    tracks_uri : str
+        URI to query for track information
+    uri : str
+        URI to query for information directly
+    """
+    def __init__(self, data: dict):
+        """Parse the data object containing playlist attributes
+
+        Parameters
+        ----------
+
+        data : dict
+            Dictionary containing attributes of a playlist
+        """
+        # Check that the JSON we are about to parse is of type "playlist"
+        if 'type' not in data.keys():
+            if data['type'] is not 'playlist':
+                raise Exception("data is not of type \"playlist\"")
+
+        # Parse out the relevant attributes
+        if 'description' in data.keys():
+            self.description = data['description']
+        if 'name' in data.keys():
+            self.name = data['name']
+        if 'owner' in data.keys():
+            self.owner = data['owner']
+        if 'tracks' in data.keys():
+            self.tracks_size = data['tracks']['total']
+            self.tracks_uri = data['tracks']['href']
+        if 'href' in data.keys():
+            self.uri = data['href']
+
+    def __len__(self):
+        return self.tracks_size
+
+    def __repr__(self):
+        return f"[Playlist: {self.name} ({self.tracks_size} tracks)]"
+
+    def get_tracks(self, oauth: OAuth2Session):
+        """For this playlist, retrieve all track information
+
+        Paramters
+        ---------
+        oauth : object
+            OAuth2 session object to use for querying for the tracks for this
+            playlist.
+
+        Returns
+        ------
+        bool
+            A boolean value. `True` if all tracks have been successfully
+            retrieved; `False` otherwise.
+        """
+        ret = False
+
+        if oauth is not None:
+            response = oauth.get(self.tracks_uri)
+
+            # TODO: Check that the track number returned in the response matches
+            # the number expected in self.track_size.
+
+            with open("trackdata.json", "w") as f:
+                import json
+                json.dump(response.json(), f, indent=True)
+        
+        return True
+
 
 def spotify_login():
     """A function to log a user into Spotify.
@@ -60,7 +146,7 @@ def spotify_login():
     return spotify, token
 
 
-def spotify_get_playlists(oauth: object, token: object, user: str) -> list:
+def spotify_get_playlists(oauth: OAuth2Session, user: str) -> list:
     """A function to get all playlists for a user.
     
     Using the given `spotify` object, retrieve all playlists associated with
@@ -76,17 +162,43 @@ def spotify_get_playlists(oauth: object, token: object, user: str) -> list:
     list
         List of spotify playlists for the given Spotify account.
     """
-    playlists = None
+    playlists = list()
+    num_playlists = 0
 
-    playlists = oauth.get(SPOTIFY_BASE_URL + f'/users/{user}/playlists')
+    response = oauth.get(SPOTIFY_BASE_URL + f'/users/{user}/playlists')
 
-    print(f"[+] playlists type: {type(playlists)}")
-    print(f"[+] JSON: {playlists.json()}")
+    print(f"[+] JSON: {response.json()}")
+
+    #import json
+    #with open("testdata.json", "w") as f:
+    #    json.dump(playlists.json(), f, indent=True)
+
+    if response.status_code == 200:
+        # Extract the JSON object from the response.
+        data = response.json()
+        
+        # Check the number of playlists in the response against the "total" number
+        # expected.       
+        expected_size = data['total']
+        total_size = len(data['items'])
+
+        if expected_size == total_size:
+            for item in data['items']:
+                print(f"Item: {item}")
+                playlists.append(Playlist(item))
+            
+            print(f"[+] Found {len(playlists)} playlists")
+
+        else:
+            print(f"[!] Expected {expected_size} playlists, data contained {total_size}")
+        
+    else:
+        print(f"[!] Playlist request failed with HTTP status code {response.status_code}")
 
     return playlists
 
 
-def spotify_get_songs(spotify: object, playlists: list):
+def spotify_get_tracks(oauth: OAuth2Session, playlists: list):
     """A function to retrive the songs for a Spotify playlist.
 
     Given the Spotify object and list of playlists, retrieve all songs
@@ -94,18 +206,18 @@ def spotify_get_songs(spotify: object, playlists: list):
 
     Parameters
     ----------
-    spotify : object
-        Spotify object needed to make the requests for each playlist.
+    oauth : object
+        Oauth2 session object to make the requests for each playlist.
     playlists : list
-        List of playlists for the given Spotify object to request their song
+        List of `Playlists` for the given Spotify object to request their song
         information.
     """
-    pass
+    for playlist in tqdm(playlists):
+        playlist.get_tracks(oauth)
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(DESCRIPTION)
-    parser.add_argument('-p', '--password', help=HELP_P)
     parser.add_argument('-u', '--user', help=HELP_U)
     
     return parser.parse_args()
@@ -129,14 +241,11 @@ def main():
     print(f"[+] Token: {spotify_token}")
     
     # 2. Get all playlists that can be retrieved for the account.
-    playlists = spotify_get_playlists(session, spotify_token, args.user)
+    playlists = spotify_get_playlists(session, args.user)
 
-    print(f"[+] Playlists: {playlists}")
-
-    ## 3. Retrieve all songs for each playlist found.
-    #if len(playlists) > 0:
-    #    num_retrieved = spotify_get_songs(spotify, playlists)
-    #    print("[+] Retrieved {num_retrieved} playlists")
+    # 3. Retrieve all songs for each playlist found.
+    if len(playlists) > 0:
+        spotify_get_tracks(session, playlists)
 
     # 4. For now, print the results as a dictionary/JSON object.
 
